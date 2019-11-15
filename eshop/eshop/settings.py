770e189a643b0a8ccdc3336a45d1f41dd6da803e
Pabijonavicius <lukas.pabijonavicius@gmail.com>
2019-11-14 19:19:04 -0500

    Fixed required dependencies

--- eshop/eshop/settings.py
+++ eshop/eshop/settings.py
@@ -37,9 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'mptt',
     'frontend',
     'products.apps.ProductsConfig',
-    'mptt',
+   
 ]
 
 MIDDLEWARE = [
