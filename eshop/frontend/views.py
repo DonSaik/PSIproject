5746a30be2641ecb2813c4dbc0124a168f4ee2fb
Pabijonavicius <lukas.pabijonavicius@gmail.com>
2019-11-14 22:33:49 -0500

    Dynamic categories and products for front-end

--- eshop/frontend/views.py
+++ eshop/frontend/views.py
@@ -1,9 +1,27 @@
 from django.shortcuts import render, HttpResponse
+from products import views as product_helpers
+
 
 # Create your views here.
 
-def home(request):
-  return render(request, 'frontend/home.html')
+def home(request, products=None):
+
+  if not products:
+    products = product_helpers.get_products(request)
+    categories = product_helpers.get_categories(request)
+
+
+  return render(request, 'frontend/home.html', {
+    "products": products,
+    "categories": categories
+    })
+
+
+
+def filter_by_category(request, category_id):
+  products = product_helpers.filter_by_category(request,category_id)
+  return render(request, 'frontend/home.html', {"products": products})
+
 
 def product_info(request, product_id):
   data = {
--- eshop/products/urls.py
+++ eshop/products/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 app_name = 'products'
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('<int:product_id>', views.detail, name='product-details'),
-    path('filter/<int:category_id>', views.filter_by_category, name='product-filter'),
+    # path('', views.index, name='index'),
+    # path('<int:product_id>', views.detail, name='product-details'),
+    # path('filter/<int:category_id>', views.filter_by_category, name='product-filter'),
 ]
